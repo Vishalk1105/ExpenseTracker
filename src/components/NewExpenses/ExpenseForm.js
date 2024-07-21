2d55c9b4b9da5de54b149cfdea9247fbf6e2e47d
vishal vilas kamble <vishalvkamble95@gmail.com>
2024-07-21 11:09:42 +0530

    Date changes

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -48,8 +48,8 @@ const ExpenseForm = (props) => {
           <label>Date</label>
           <input
             type="date"
-            min="2019-01-01"
-            max="2022-12-31"
+            min="2021-01-01"
+            max="2024-12-31"
             onChange={dateHandler}
             value={enteredDate}
           />
